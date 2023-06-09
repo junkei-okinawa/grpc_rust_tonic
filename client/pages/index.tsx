3fed1bec5787db518fecf7963469f7244e1254f1
junkei_okinawa <ceazy.x2.okinawan@gmail.com>
2023-06-10 05:26:40 +0900

    first commit

---
+++ client/pages/index.tsx
@@ -0,0 +1,42 @@
+import { createGrpcWebTransport } from "@bufbuild/connect-web";
+import { createPromiseClient } from "@bufbuild/connect";
+import type { PartialMessage } from "@bufbuild/protobuf";
+import { type FormEvent, useState } from "react";
+import { Greeter } from "../services/greeting_connectweb";
+// messageスキーマの型情報もimportすることで利用可能
+import type { GreetingMessage, Person } from "../services/greeting_pb";
+
+// gRPCクライアントの初期化
+const transport = createGrpcWebTransport({
+  baseUrl: "http://localhost:50051",
+});
+const client = createPromiseClient(Greeter, transport);
+
+export default function Home() {
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    // リクエストメッセージのオブジェクトはPartialMessageを使うと取れます
+    const person: PartialMessage<Person> = { name };
+    // gRPCメソッドを呼び出す
+    const greetingMessage: GreetingMessage = await client.sayHello(person);
+    console.log("greetingMessage: ", greetingMessage);
+    setText(greetingMessage.text);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
+      <div>{text}</div>
+    </>
+  );
+}
